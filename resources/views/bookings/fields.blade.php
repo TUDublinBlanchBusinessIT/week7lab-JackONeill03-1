a46c666fba74db694ef472b46304ac5d61836e15
B00155573 Jack O Neill <B00155573@mytudublin.ie>
2025-03-10 16:30:11 +0000

    Finished Third & Fourth Page

--- resources/views/bookings/fields.blade.php
+++ resources/views/bookings/fields.blade.php
@@ -16,10 +16,8 @@
     {!! Form::text('endtime', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Memberid Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('memberid', 'Memberid:') !!}
-    {!! Form::number('memberid', null, ['class' => 'form-control']) !!}
+<input type="hidden" name="memberid" value="{{Auth::user()->member->id}}">
 </div>
 
 <!-- Courtid Field -->
